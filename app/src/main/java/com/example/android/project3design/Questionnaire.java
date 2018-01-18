bcb24538e4dcead4b611ba0b9e8040e0c09721fc
RivkaFrishman <rivka316@gmail.com>
2018-01-18 13:04:45 +0200

    ew

--- app/src/main/java/com/example/android/project3design/Questionnaire.java
+++ app/src/main/java/com/example/android/project3design/Questionnaire.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Questionnaire extends AppCompatActivity {
 
@@ -16,8 +18,28 @@ public class Questionnaire extends AppCompatActivity {
         Button nextButton = findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DoorsWindows.class);
-                startActivity(intent);
+                EditText wallAEditText = findViewById(R.id.wallAwidthValue);
+                EditText wallBEditText = findViewById(R.id.wallBwidthValue);
+                EditText wallCEditText = findViewById(R.id.wallCwidthValue);
+                EditText wallDEditText = findViewById(R.id.wallDwidthValue);
+                EditText wallHeightEditText = findViewById(R.id.wallHeightValue);
+
+                String strWallAEditText = wallAEditText.getText().toString();
+                String strWallBEditText = wallBEditText.getText().toString();
+                String strWallCEditText = wallCEditText.getText().toString();
+                String strWallDEditText = wallDEditText.getText().toString();
+                String strWallHeightEditText = wallHeightEditText.getText().toString();
+
+                if (!strWallAEditText.matches("") || !strWallBEditText.matches("") ||
+                        !strWallCEditText.matches("") || !strWallDEditText.matches("") ||
+                        !strWallHeightEditText.matches("")) {
+                    Intent intent = new Intent(getApplicationContext(), DoorsWindows.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Please fill in the requested values to continue", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         });
     }
